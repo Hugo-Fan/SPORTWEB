89328267a8ba59297e5d1284eb36fadb555ce6fc
Hugo <test@gmail.com>
2026-02-04 00:15:35 +0800

    上傳檔案

--- src/main/java/com/hugo/blog/settings/MvcSettings.java
+++ src/main/java/com/hugo/blog/settings/MvcSettings.java
@@ -13,11 +13,11 @@ public class MvcSettings implements WebMvcConfigurer {
     }
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/view/addArticle").setViewName("/blog/addArticle");
-        registry.addViewController("/view/userlogin").setViewName("/blog/userLogin");
-        registry.addViewController("/view/forgetpasword").setViewName("/blog/forgetpasword");
-        registry.addViewController("/view/addUser").setViewName("/blog/addUser");
-        registry.addViewController("/view/addSportList").setViewName("/blog/addSportList");
-        registry.addViewController("/view/introduce").setViewName("/blog/introduce");
+        registry.addViewController("/view/addArticle").setViewName("blog/addArticle");
+        registry.addViewController("/view/userlogin").setViewName("blog/userLogin");
+        registry.addViewController("/view/forgetpasword").setViewName("blog/forgetpasword");
+        registry.addViewController("/view/addUser").setViewName("blog/addUser");
+        registry.addViewController("/view/addSportList").setViewName("blog/addSportList");
+        registry.addViewController("/view/introduce").setViewName("blog/introduce");
     }
 }
